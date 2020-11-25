1f94891b151d90ba50d812893af4728187302e98
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-11-25 02:56:30 +0100

    gpio and pwm flags

--- include/gpio.h
+++ include/gpio.h
@@ -1,4 +1,8 @@
 #pragma once
 
-#define ACTIVE_HIGH	0
-#define ACTIVE_LOW	1
+#define GPIO_ACTIVE_HIGH	0
+#define GPIO_ACTIVE_LOW		1
+
+// backwards compatibility
+#define ACTIVE_HIGH	GPIO_ACTIVE_HIGH
+#define ACTIVE_LOW	GPIO_ACTIVE_LOW
---
+++ include/pwm.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PWM_ACTIVE_HIGH	0
+#define PWM_ACTIVE_LOW	1
