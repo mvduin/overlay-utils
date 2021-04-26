a8c4e3ac21ea44df13dee99694663537680c407a
Matthijs van Duin <matthijsvanduin@gmail.com>
2021-04-27 01:52:54 +0200

    fix/revise spi0 example

--- include/bone/common.h
+++ include/bone/common.h
@@ -3,3 +3,7 @@
 
 // macro to disable pinmux helper node (runtime pinmux):
 #define USES_PIN( pin ) &ocp { pin##_pinmux { status = "disabled"; }; }
+
+// macro to disable spi channel created by cape-universal
+#define _USES_SPI( cs ) cs { status = "disabled"; }
+#define USES_SPI( bus, cs ) &spi##bus { channel@##_USES_SPI(cs); spi##bus##channel@##_USES_SPI(cs); }
