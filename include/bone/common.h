d4c24d2a91358575c936cfb6816a04a238f034bb
Matthijs van Duin <matthijsvanduin@gmail.com>
2022-03-03 21:20:37 +0100

    add adc.dtsi

--- include/bone/common.h
+++ include/bone/common.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "subarctic/pinctrl.h"
 
-// macro to disable pinmux helper node (runtime pinmux):
+// macro to disable pinmux helper node created by cape-universal
 #define USES_PIN( pin ) &ocp { pin##_pinmux { status = "disabled"; }; }
+
+// macro to disable child of gpio-of-helper created by cape-universal
+// (supported since kernel 4.19.94-ti-r69)
+#define USES_GPIO( pin ) &ocp { cape-universal { pin { status = "disabled"; }; }; }
 
 // macro to disable spi channel created by cape-universal
 #define _USES_SPI( cs ) cs { status = "disabled"; }
