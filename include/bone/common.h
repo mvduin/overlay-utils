ebf3b963ad2d83ed950eec19e3d87013b15a64ed
Matthijs van Duin <matthijsvanduin@gmail.com>
2022-03-19 18:40:24 +0100

    fix USES_GPIO and merge it into USES_PIN
    It doesn't make sense to claim the gpio without claiming the pin or vice
    versa, so merge these macros.
    
    Also implement a gross workaround for the problem of gpio node names
    getting mangled because they match the names of pin macros.  The nice
    solution would be to enhance dtc to support numerical constants that can
    be used in property values, instead of using cpp macros which are
    expanded in every context.

--- include/bone/common.h
+++ include/bone/common.h
@@ -2,11 +2,8 @@
 #include "subarctic/pinctrl.h"
 
 // macro to disable pinmux helper node created by cape-universal
-#define USES_PIN( pin ) &ocp { pin##_pinmux { status = "disabled"; }; }
-
-// macro to disable child of gpio-of-helper created by cape-universal
-// (supported since kernel 4.19.94-ti-r69)
-#define USES_GPIO( pin ) &ocp { cape-universal { pin { status = "disabled"; }; }; }
+// also disable child of gpio-of-helper created by cape-universal (supported since kernel 4.19.94-ti-r69)
+#define USES_PIN( pin ) &ocp { pin##_pinmux { status = "disabled"; }; cape-universal { __quote_for_cpp_##pin { status = "disabled"; }; }; }
 
 // macro to disable spi channel created by cape-universal
 #define _USES_SPI( cs ) cs { status = "disabled"; }
